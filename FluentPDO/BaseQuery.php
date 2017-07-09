188f20b054983ddf96a65af0ee02c68477fa9457
Mike Stevens <mistevs@gmail.com>
2017-07-09 11:31:26 -0400

    adding method to get a fully built query

--- FluentPDO/BaseQuery.php
+++ FluentPDO/BaseQuery.php
@@ -139,7 +139,7 @@ abstract class BaseQuery implements IteratorAggregate
         if ($result && $result->execute($parameters)) {
             echo 'dumping PDO';
             var_dump($result);
-            var_dump($result->debugDumbParams());
+            var_dump($result->debugDumpParams());
             exit;
             $this->time = microtime(true) - $time;
             $this->result = $result;
